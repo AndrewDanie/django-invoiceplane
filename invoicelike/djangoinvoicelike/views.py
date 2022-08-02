47acd6c55369df098b5018bb3e22bfc8aeaaa5f2
AndrewDanie <103277927+AndrewDanie@users.noreply.github.com>
2022-08-02 15:59:07 +0300

    Add plotly dash

--- invoicelike/djangoinvoicelike/views.py
+++ invoicelike/djangoinvoicelike/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from .models import *
 import datetime
 
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+
+
 
 def main_page(request):
     if request.method == 'POST':
@@ -29,8 +33,22 @@ def main_page(request):
     names = [f.verbose_name.title() for f in Vehicle._meta.get_fields()][1:]
     data['income'] = Vehicle.objects.values('sale_date', 'price').order_by('sale_date')
 
+    x_data = [0, 1, 2, 3]
+    y_data = [x ** 2 for x in x_data]
+    plot_sold_cars = plot([Scatter(x=x_data, y=y_data,
+                             mode='lines', name='test',
+                             opacity=0.9, marker_color='green')],
+                    output_type='div')
+
+    plot_kia = plot([Scatter(x=x_data, y=y_data,
+                                   mode='lines', name='test',
+                                   opacity=0.9, marker_color='green')],
+                          output_type='div')
+
     return render(request, 'djangoinvoicelike/index.html',
                   {'data': data,
                    'names': names,
-                   'dash_types': user_dashboards}
-                  )
+                   'dash_types': user_dashboards,
+                   'plot_sold_cars': plot_sold_cars,
+                   'plot_kia': plot_kia,
+                })
